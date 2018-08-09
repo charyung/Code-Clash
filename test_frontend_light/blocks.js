29163d320311f3a99001f4397bd1d55a8f785656
charyung <charmaineyung@yahoo.com.hk>
2018-08-08 21:28:07 -0400

    Syntax highlighting!!!
    It works, finally! Kind of. The block that pops up still doesn't have it.

--- test_frontend_light/blocks.js
+++ test_frontend_light/blocks.js
@@ -29,7 +29,7 @@ class CodeBlock extends React.Component
 		//We need some kind of codeblock here. I found syntaxhighlighter, but figure that out later.
 		//return (<pre style={{display: "inline"}} className={this.props.lang}> <code className={this.props.class} style={this.state.s} onClick={this.props.click}> {this.props.code} </code> </pre>)
 		return (
-			<pre style={{display: "inline"}}>
+			<pre style={{display: "inline", textAlign: "left"}}>
 				<code className={this.props.class} onClick={this.props.click}>
 {this.props.code}
 				</code>
@@ -71,22 +71,30 @@ class UI extends React.Component
 		
 		this.openOverlay = this.openOverlay.bind(this);
         this.closeOverlay = this.closeOverlay.bind(this);
-		
-		var ReactDOMServer = React.__SECRET_DOM_SERVER_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
-		console.log(ReactDOMServer);
-		//var test = ReactDOMServer.renderToStaticMarkup(<pre><code className="language-css">{`p { color: red }`}</code></pre>);
     }
 	
 	//These two functions makes it so whenever you click anywhere (that isn't the box in the middle) in the whole page, the overlay closes.
 	componentDidMount()
 	{	
         document.addEventListener("click", this.closeOverlay);
+		this.highlightSyntax();
     }
 
     componentWillUnmount()
 	{
         document.removeEventListener("click", this.closeOverlay);
     }
+	
+	//To be honest, I don't really understand this code 100%. It's from here: https://labs.chiedo.com/blog/using-google-code-prettify-reactjs/
+	highlightSyntax()
+	{
+		var script = document.createElement('script');
+		script.type = 'text/javascript';
+		//script.async = true;
+		
+		script.src = 'https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js';
+		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+	}
 
     openOverlay(e)
 	{
@@ -96,7 +104,7 @@ class UI extends React.Component
 		
         const style = null;
 		//This gets the text of the clicked target directly. I know this breaks abstraction, and it will be fixed soon(tm).
-        this.setState({ style: style, blockValue: e.target.textContent });
+        this.setState({ style: style, blockValue: e.target.parentNode.textContent });
 		console.log(e.target);
         //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
         document.addEventListener("click", this.closeOverlay);
@@ -125,12 +133,28 @@ class UI extends React.Component
 			<div align="center">
 				<div className="overlay" style={this.state.style}>
 					<div ref={this.setBlockWrapper} style={{height: 0}}>
-						<CodeBlock class="overlayBlock" code={this.state.blockValue}/>
+						<CodeBlock class="overlayBlock prettyprint linenums" code={this.state.blockValue}/>
 					</div>
 				</div>
-				<CodeBlock class="block language-python" code="import math" click={this.openOverlay} /> <CodeBlock class="block language-python" code="import random" click={this.openOverlay}/>
+				<CodeBlock class="block prettyprint" code="if (true)
+{
+	//Testing
+	var i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		//do something
+	}
+}
+	}
+}
+}
+}
+}
+}
+
+					" click={this.openOverlay} /> <CodeBlock class="block prettyprint" code="import random" click={this.openOverlay}/>
 				<br />
-				<div align="center" style={{position: "relative"}}>
+				<div style={{position: "relative"}}>
 					<button> &lt; </button> Vote! <button> &gt; </button>
 				</div>
 			</div>
