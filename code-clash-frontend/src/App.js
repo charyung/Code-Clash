b1257113a47f2df11ed77be953ba45de69e082ec
charyung <charmaineyung@yahoo.com.hk>
2018-10-05 13:05:59 -0400

    I realized that I misspelled the name of the project

--- code-clash-frontend/src/App.js
+++ code-clash-frontend/src/App.js
@@ -5,35 +5,48 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 const Header = () => (
 	<Router>
 		<div>
-		<div className="headerWrapper">
-			<div className="headerClass">
-				<h1> Code Clash </h1>
-				<Link to="/">Home</Link>
-				<Link to="/about">About</Link>
-				<Link to="/topics">Topics</Link>
-			</div>
-		</div>
-				<Route exact path="/" component={Home} />
-				<Route path="/about" component={About} />
-				<Route path="/topics" component={Topics} />
+			<Route exact path="/" component={Home} />
+			<Route path="/about" component={About} />
+			<Route path="/topics" component={Topics} />
 		</div>
 	</Router>
 );
 
 const Home = () => (
     <div>
+		<div className="headerWrapper">
+			<div className="headerClass">
+				<h1> Code Clash </h1>
+				<Link to="/about">About</Link>
+				<Link to="/topics">Topics</Link>
+			</div>
+		</div>
         <h2>Home</h2>
     </div>
 );
 
 const About = () => (
 	<div>
+		<div className="headerWrapper">
+			<div className="headerClass">
+				<h1> Code Clash </h1>
+				<Link to="/">Home</Link>
+				<Link to="/topics">Topics</Link>
+			</div>
+		</div>
 		<h2>About</h2>
 	</div>
 );
 
 const Topics = ({ match }) => (
 	<div>
+		<div className="headerWrapper">
+			<div className="headerClass">
+				<h1> Code Clash </h1>
+				<Link to="/">Home</Link>
+				<Link to="/about">About</Link>
+			</div>
+		</div>
 		<h2>Topics</h2>
 		<ul>
 			<li>
